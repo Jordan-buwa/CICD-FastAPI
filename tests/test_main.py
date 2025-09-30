94c00bd40c9a4c001e0212a66e0baabaf1c291e0
Your Name <your.email@example.com>
2025-09-30 12:36:59 +0000

    Managing pbs 1

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
 from unittest.mock import MagicMock
-
+import sys
+from pathlib import Path
+# Add the project root to sys.path
+sys.path.append(str(Path(__file__).parent.parent))
 import pytest
 from fastapi.testclient import TestClient
 
